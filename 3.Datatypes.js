c21d85fbc180a9d8ae8bc9b0d8079c5991db9cf7
ANKAN MAITY <mr.ankanmaity@gmail.com>
2024-03-08 19:06:18 +0000

    working on primitive datatypes

--- 3.Datatypes.js
+++ 3.Datatypes.js
@@ -1,11 +1,25 @@
 // JavaScript has 8 Datatypes
 // 1. String - textual data
+let color = "yellow";
+
 // 2. Number - numbers
+let num = 120;
+
 // 3. Bigint - new data types to represent integers of arbitrary                length
+let bigInt = 1234567890123456789012345678901234567890n;
+
 // 4. Boolean - true or false
+let isTrue = false;
+
 // 5. Undefined - return statement with no value
+let a;
+
 // 6. Null - nulled value
-// 7. Symbol -
+let b = null;
+
+// 7. Symbol - unique value
+let sym = Symbol("unique symbol");
+
 // 8. Object
 
 // The Object Datatype
@@ -15,3 +29,11 @@
 // 1. An object
 // 2. An array
 // 3. A date
+
+console.log(color);
+console.log(num);
+console.log(bigInt);
+console.log(isTrue);
+console.log(a);
+console.log(b);
+console.log(sym);
