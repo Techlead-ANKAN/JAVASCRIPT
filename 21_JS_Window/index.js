2326d861088757274805c113c75d92d232569fab
ANKAN MAITY <mr.ankanmaity@gmail.com>
2024-07-06 13:27:51 +0530

    Done with half of WIndow Object Properties.

--- 21_JS_Window/index.js
+++ 21_JS_Window/index.js
@@ -1,2 +1,83 @@
 // Window Object
 
+// The Window object represents an open window in a browser that contains a DOM Document. They have some properties as well as methods to manipulate  the document, handling events, managing timers, etc. 
+
+
+// Window Object Properties  [ link: https://www.w3schools.com/jsref/obj_window.asp ]
+
+
+// 1) <window_var>.closed   ===>    It is a read-only property that returns true if the window is closed.
+// console.log(<window_var>.closed);
+
+
+// 2) <window_var>.frameElement     ===>    It is a read-only property that returns the frame where the window is running, if the window ius not running in a frame then it returns "NULL".
+// console.log(<window_var>.frameElement);
+
+
+// 3) <window_var>.frames   ===>    It is a property that returns an array with all window objects in the window. The winows can be accessed by index numbers.
+// console.log(<window_var>.frames[1].location); // getting location of the window present in the second frame
+// <window_var>.frames[0].location = "https://www.w3schools.com/jsref/";  // setting the location of the window present in the first frame.
+
+
+// 4) -----------------Window History Object--------------------
+
+// The history object contains the URLs visited by the user (in the vbrowser window). It is a property of the window object.
+// Accessed with: window.history
+
+// Properties and methods:
+
+// (i) length  --->  The length property returns the number of URLs in the history list of the current browser window. It returns atleast 1.
+// console.log(window.history.length);
+
+// (ii) back()  --->  The history.back() method loads the previous URL (page) in the history list, and works only if a previous page exists.
+// history.back();
+
+// (iii) forward()  --->  The history.forward() method loads the next URL (page) in the history list, and works only if a next page exists.
+// history.forward();
+
+// (iv) go(page_number)   --->  The history.go() method loads a URL (page) from the history list and only works if the page exist in the history list.
+// history.go(0);   ---> Reloads the page
+// history.go(-1);  ---> Same as back()
+// history.go(1);   ---> Same as forward()
+// history.go(2);   ---> Loads the second page in the history
+
+// -----------------------------------------------------------------------
+
+
+// 5) <window_var>.innerHeight  ===>    It is a read-only property that returns height of the window's content area.
+// console.log(window.innerHeight);
+
+
+// 6) <window_var>.innerWidth  ===>    It is a read-only property that returns width of the window's content area.
+// console.log(window.innerWidth);
+
+
+// 7) <window_var>.length   ===>    It is a read-only property that returns the no.of framed windows inside a window and can be accessed by using index no.
+// console.log(window.length);
+
+
+// 8) localStorage  ===>    This object allows you to store key-value pairs in the browser. There is no expiration date for the data it stores. The data is not deleted when the browser is closed and are available for future sessions. 
+// localStorage.setItem(<key>, <value>);
+// localStorage.getItem(key);
+// localStorage.removeItem(<key>, <value>);
+// localStorage.clear;
+
+
+// 9) window.name   ===> This property sets or returns the name of a window.
+// console.log(window.name)
+// window.name = "Ankan's Window"
+
+
+// 10) window.opener    ===>    The opener returns a reference to the window that created the window.
+// console.log(window.opener);
+
+
+// 11) <window_var>.outerHeight  ===>    It is a read-only property that returns height of the window including all.
+// console.log(window.outerHeight);
+
+
+// 12) <window_var>.outerWidth  ===>    It is a read-only property that returns width of the window including all.
+// console.log(window.outerWidth);
+
+
+// 13) 
